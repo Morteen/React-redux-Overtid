09bfb35bd8ad22ffc26078df34ecd8e5ba227b77
Unknown <mortenolsen4@gmail.com>
2018-09-06 14:16:09 +0200

    Signup form og state

---
+++ overtid/src/client/component/signup/SignupForm.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react'
+
+export default class SignupForm extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            username:"",
+            navn:"",
+            passord:"",
+            passordConf:""
+        }
+        this.onChange=this.onChange.bind(this);
+        this.onSubmit=this.onSubmit.bind(this);
+    }
+    onChange(e){
+        this.setState({[e.target.name]:e.target.value});
+    }
+    onSubmit(e){
+        e.preventDefault();
+        console.log(this.state)
+    }
+  render() {
+    return (
+      <form onSubmit={this.onSubmit}>
+          <h1>Meld deg inn hos overtids haiene</h1>
+
+          <div className="form-group">
+          <label className="control-label">Navn</label>
+          <input 
+          value={this.state.unavn}
+          onChange={this.onChange}
+          type="text" 
+          name="navn"
+          className="form-control"
+          />
+          </div>
+          <div className="form-group">
+          <label className="control-label">Brukernavn</label>
+          <input 
+          value={this.state.username}
+          onChange={this.onChange}
+          type="text" 
+          name="username"
+          className="form-control"
+          />
+          </div>
+          <div className="form-group">
+          <label className="control-label">Passord</label>
+          <input 
+          value={this.state.passord}
+          onChange={this.onChange}
+          type="text" 
+          name="passord"
+          className="form-control"
+          />
+          </div>
+          <div className="form-group">
+          <label className="control-label">Bekreft passord</label>
+          <input 
+          value={this.state.passordConf}
+          onChange={this.onChange}
+          type="text" 
+          name="passordConf"
+          className="form-control"
+          />
+          </div>
+
+          <div className="form-group">
+          <button className="btn btn-primary">Sign in</button>
+          </div>
+        
+      </form>
+    )
+  }
+}
